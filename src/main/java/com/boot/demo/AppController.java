4f816719a9bcc167916051e9c7f3f791983b6424
ashish.sawant-ext@rexel.com <c2Wr4w2H>
2020-07-27 00:29:50 +0530

    Oauth2 with github

--- src/main/java/com/boot/demo/AppController.java
+++ src/main/java/com/boot/demo/AppController.java
@@ -1,10 +1,14 @@
 package com.boot.demo;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class AppController {
+@EnableOAuth2Sso
+public class AppController extends WebSecurityConfigurerAdapter  {
 	
 	
 	@RequestMapping("/home")
@@ -12,5 +16,16 @@ public class AppController {
 	{
 		return "homepage";
 	}
+	
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+          .authorizeRequests()
+            .antMatchers("/login**")
+            .permitAll()
+          .anyRequest()
+            .authenticated();
+    }
 
 }
